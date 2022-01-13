3d597c6d8536bbbc51ef69a8ab5b32d60f9da019
Danielgdm-vlr <daniel.dany16@yahoo.com>
2022-01-13 23:33:02 +0200

    2nd commit - app skeleton complete i think

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddBookComponent } from './components/add-book/add-book.component';
+import { BookComponent } from './components/book/book.component';
+import { HomeComponent } from './components/home/home.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'book', component: BookComponent },
+  { path: 'book/add', component: AddBookComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
